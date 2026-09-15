f8e015751f428fd26189b8a6e4ce8fd5d839e663
beffaxx <gamergabriele.19994@gmail.com>
2026-09-15 18:02:23 +0200

    Aggiornati testi e immagini pagina approfondimenti 2

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,9 +27,6 @@ export const metadata: Metadata = {
 }
 
 export const viewport: Viewport = {
-  width: 'device-width',
-  initialScale: 1,
-  maximumScale: 1,
   colorScheme: 'light dark',
   themeColor: [
     { media: '(prefers-color-scheme: light)', color: 'white' },
